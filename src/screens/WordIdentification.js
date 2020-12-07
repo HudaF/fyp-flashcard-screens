b4f81b4342140012ab9a49bf2e12fe17c8da48a2
hf04097 <hf04097@st.habib.edu.pk>
2020-12-07 18:01:11 +0500

    screen check1

--- src/screens/WordIdentification.js
+++ src/screens/WordIdentification.js
@@ -25,32 +25,33 @@ function WordIdentification(props) {
           ></Image>
         </TouchableOpacity>
         <View style={styles.buttonFiller}></View>
-        <TouchableOpacity style={styles.button1}>
-          <View style={styles.image3Filler}></View>
+        <View style={styles.button1Stack}>
+          <TouchableOpacity style={styles.button1}></TouchableOpacity>
           <Image
             source={require("../assets/images/aam.jpg")}
             resizeMode="contain"
             style={styles.image3}
           ></Image>
-        </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.button2Row}>
-        <TouchableOpacity style={styles.button2}>
+      <View style={styles.button2StackRow}>
+        <View style={styles.button2Stack}>
+          <TouchableOpacity style={styles.button2}></TouchableOpacity>
           <Image
             source={require("../assets/images/keela_image.jpg")}
             resizeMode="contain"
             style={styles.image4}
           ></Image>
-        </TouchableOpacity>
-        <View style={styles.button2Filler}></View>
-        <TouchableOpacity style={styles.button3}>
-          <View style={styles.image5Filler}></View>
+        </View>
+        <View style={styles.button2StackFiller}></View>
+        <View style={styles.button3Stack}>
+          <TouchableOpacity style={styles.button3}></TouchableOpacity>
           <Image
             source={require("../assets/images/grapes.png")}
             resizeMode="contain"
             style={styles.image5}
           ></Image>
-        </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -93,89 +94,106 @@ const styles = StyleSheet.create({
     borderColor: "#000000"
   },
   image2: {
-    width: 150,
     borderWidth: 2,
     borderColor: "#000000",
     borderRadius: 12,
-    flex: 1
+    width: 150,
+    flex: 1,
+    marginBottom: 1
   },
   buttonFiller: {
     flex: 1,
     flexDirection: "row"
   },
   button1: {
+    position: "absolute",
     backgroundColor: "rgba(255,191,8,1)",
-    height: 203,
+    right: 1,
     borderRadius: 12,
     borderWidth: 0,
     borderColor: "#000000",
-    width: 150,
-    flexDirection: "row"
-  },
-  image3Filler: {
-    flex: 1,
-    flexDirection: "row"
+    top: 0,
+    bottom: 0,
+    width: 150
   },
   image3: {
-    height: 202,
+    position: "absolute",
     borderWidth: 2,
     borderColor: "#000000",
     borderRadius: 12,
+    right: 0,
+    top: 0,
+    bottom: 1,
     width: 150
+  },
+  button1Stack: {
+    width: 151
   },
   buttonRow: {
     height: 203,
     flexDirection: "row",
-    marginTop: 78,
-    marginLeft: 30,
-    marginRight: 17
+    marginTop: 28,
+    marginLeft: 24,
+    marginRight: 23
   },
   button2: {
+    top: 1,
+    left: 1,
+    position: "absolute",
     backgroundColor: "rgba(255,191,8,1)",
     width: 150,
-    height: 203,
-    borderRadius: 12,
     borderWidth: 0,
-    borderColor: "#000000"
+    borderColor: "#000000",
+    bottom: 0
   },
   image4: {
+    top: 0,
+    left: 0,
     width: 150,
-    height: 202,
+    position: "absolute",
     borderWidth: 2,
     borderColor: "#000000",
     borderRadius: 12,
-    marginTop: 1
+    bottom: 2
   },
-  button2Filler: {
+  button2Stack: {
+    width: 151
+  },
+  button2StackFiller: {
     flex: 1,
     flexDirection: "row"
   },
   button3: {
+    top: 1,
+    position: "absolute",
     backgroundColor: "rgba(255,191,8,1)",
-    height: 203,
+    right: 0,
     borderRadius: 12,
     borderWidth: 1,
     borderColor: "#000000",
-    width: 150,
-    flexDirection: "row"
-  },
-  image5Filler: {
-    flex: 1,
-    flexDirection: "row"
+    bottom: 0,
+    width: 150
   },
   image5: {
+    top: 0,
     width: 150,
+    position: "absolute",
     borderWidth: 2,
     borderColor: "#000000",
     borderRadius: 12,
-    height: 203
+    right: 0,
+    bottom: 0
   },
-  button2Row: {
-    height: 203,
+  button3Stack: {
+    width: 150,
+    marginBottom: 1
+  },
+  button2StackRow: {
+    height: 204,
     flexDirection: "row",
     marginTop: 15,
-    marginLeft: 30,
-    marginRight: 17
+    marginLeft: 23,
+    marginRight: 24
   }
 });
 
